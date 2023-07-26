3a457c5b79cdabc2019e176f2280c87db870e731
nash1111 <ken1251213@gmail.com>
2023-07-27 01:40:07 +0900

    wip

--- src/lib.rs
+++
@@ -1,8 +0,0 @@
-extern crate chrono;
-use chrono::{NaiveDateTime, Utc, TimeZone};
-
-pub fn parse_unix_timestamp(unix_timestamp: i64) -> String {
-    let datetime = NaiveDateTime::from_timestamp(unix_timestamp, 0);
-    let utc_datetime = Utc.from_utc_datetime(&datetime);
-    utc_datetime.to_string()
-}
---
+++ src/main.rs
@@ -0,0 +1,49 @@
+extern crate chrono;
+use chrono::{NaiveDateTime, Utc, TimeZone};
+use wasi::fd_write;
+use wasi::Ciovec;
+
+
+
+#[no_mangle]
+pub extern "C" fn parse_unix_timestamp(unix_timestamp: i64) {
+    let datetime = NaiveDateTime::from_timestamp(unix_timestamp, 0);
+    let utc_datetime = Utc.from_utc_datetime(&datetime);
+    let output = utc_datetime.to_string() + "\n";
+    let data = [Ciovec {
+        buf: output.as_ptr(),
+        buf_len: output.len(),
+    }];
+    unsafe {
+        fd_write(1, &data).unwrap();
+    }
+}
+
+fn main() {
+    parse_unix_timestamp(1690385088);
+    let (argc, arg_buf_size) = unsafe { wasi::args_sizes_get().unwrap() };
+    let mut args = Vec::with_capacity(argc);
+    let mut arg_buf = Vec::with_capacity(arg_buf_size);
+    unsafe { wasi::args_get(args.as_mut_ptr(), arg_buf.as_mut_ptr()).unwrap() };
+    let args: Vec<String> = args.iter().map(|&arg_ptr| {
+        let arg_len = unsafe { libc::strlen(arg_ptr as *const i8) };
+        let arg_slice = unsafe { std::slice::from_raw_parts(arg_ptr, arg_len) };
+        String::from_utf8_lossy(arg_slice).into_owned()
+    }).collect();
+    
+
+    let output = format!("args length: {}\n", args.len());
+    let data = [Ciovec {
+        buf: output.as_ptr(),
+        buf_len: output.len(),
+    }];
+    unsafe {
+        fd_write(1, &data).unwrap();
+    }
+
+    if args.len() > 1 {
+        if let Ok(unix_timestamp) = args[1].parse::<i64>() {
+            parse_unix_timestamp(unix_timestamp);
+        }
+    }
+}
