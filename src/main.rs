95b9ee14a2351d9cbdb069a06526c58e230f9556
nash1111 <ken1251213@gmail.com>
2023-07-27 09:15:49 +0900

    chore: cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 extern crate chrono;
-use chrono::{NaiveDateTime, Utc, TimeZone};
+use chrono::{NaiveDateTime, TimeZone, Utc};
 use wasi::fd_write;
 use wasi::Ciovec;
-
-
 
 #[no_mangle]
 pub extern "C" fn parse_unix_timestamp(unix_timestamp: i64) {
@@ -26,11 +24,14 @@ fn main() {
 
     let mut arg_buf = Vec::with_capacity(arg_buf_size);
     unsafe { wasi::args_get(args.as_mut_ptr(), arg_buf.as_mut_ptr()).unwrap() };
-    let args: Vec<String> = args.iter().map(|&arg_ptr| {
-        let arg_len = unsafe { libc::strlen(arg_ptr as *const i8) };
-        let arg_slice = unsafe { std::slice::from_raw_parts(arg_ptr, arg_len) };
-        String::from_utf8_lossy(arg_slice).into_owned()
-    }).collect();
+    let args: Vec<String> = args
+        .iter()
+        .map(|&arg_ptr| {
+            let arg_len = unsafe { libc::strlen(arg_ptr as *const i8) };
+            let arg_slice = unsafe { std::slice::from_raw_parts(arg_ptr, arg_len) };
+            String::from_utf8_lossy(arg_slice).into_owned()
+        })
+        .collect();
 
     if args.len() > 1 {
         if let Ok(unix_timestamp) = args[1].parse::<i64>() {
